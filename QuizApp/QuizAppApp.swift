127f92a0f1a483004852409ff3db15af787d7611
Rion <rionfx.42@gmail.com>
2021-10-16 16:15:33 +0900

    Created Project

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by FX on 2021/10/16.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
